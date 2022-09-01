74a2510cfa0c8bb9ba009c8e993af7888c67b5ff
Anirban Saha <mailme@anirbansaha.com>
2022-09-01 18:07:04 +0530

    name == main
    gg

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
   return("Hello World. - A & S.")
+
+
+if __name__ == '__main__':
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+    app.run() 
